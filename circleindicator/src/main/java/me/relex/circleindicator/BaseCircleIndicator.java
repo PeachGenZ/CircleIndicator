46e36e0ab2fc7b3cc41393056bf9d7576ea10a45
Srithep Witayapanpracha <32310775+PeachGenZ@users.noreply.github.com>
2021-01-07 01:11:04 +0700

    change animation

--- circleindicator/src/main/java/me/relex/circleindicator/BaseCircleIndicator.java
+++ circleindicator/src/main/java/me/relex/circleindicator/BaseCircleIndicator.java
@@ -130,7 +130,7 @@ class BaseCircleIndicator extends LinearLayout {
         setOrientation(config.orientation == VERTICAL ? VERTICAL : HORIZONTAL);
         setGravity(config.gravity >= 0 ? config.gravity : Gravity.CENTER);
 
-        immediateBackToNormalAnimation = ValueAnimator.ofFloat(4f, 1f);
+        immediateBackToNormalAnimation = ValueAnimator.ofFloat(1f, 4f);
         immediateBackToNormalAnimation.setInterpolator(new FastOutSlowInInterpolator());
         immediateBackToNormalAnimation.setDuration(0);
         immediateBackToNormalAnimation.addListener(new Animator.AnimatorListener() {
@@ -198,7 +198,7 @@ class BaseCircleIndicator extends LinearLayout {
 
             }
         });
-        backToNormalAnimation = ValueAnimator.ofFloat(4f, 1f);
+        backToNormalAnimation = ValueAnimator.ofFloat(1f, 4f);
         backToNormalAnimation.setInterpolator(new FastOutSlowInInterpolator());
         backToNormalAnimation.setDuration(300);
         backToNormalAnimation.addListener(new Animator.AnimatorListener() {
